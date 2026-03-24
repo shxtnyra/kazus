370b32e2a2d1f0e684b17de01656fa223bc8cd6a
shxtnyra <est.oblivio@mail.ru>
2026-03-25 00:06:40 +0300

    mvp

---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow, Menu } = require('electron');
+
+function createWindow() {
+  Menu.setApplicationMenu(null);
+
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
